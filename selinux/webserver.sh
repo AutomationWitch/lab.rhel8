8635fc520ffa61ccce82a7d8e6585803db307516
AutomationWitch <53153683+AutomationWitch@users.noreply.github.com>
2019-09-18 22:00:57 +0200

    Create webserver.sh

---
+++ selinux/webserver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## Salut les OPS, voici le script de déploiement du nouveau site !
+## Bon week-end !
+
+yum install httpd -y
+
+mkdir /web
+echo 'Hello World ;)' > /web/index.html
+
+sed -i 's/DocumentRoot \"\/var\/www\/html\"/DocumentRoot \"\/web\"/g' /etc/httpd/conf/httpd.conf
+sed -i 's/Directory \"\/var\/www\/html\"/Directory \"\/web\"/g' /etc/httpd/conf/httpd.conf
+
+systemctl enable --now httpd
